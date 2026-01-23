f24bd2bd0b02e0c5350bb5d938d3dfbeece41b80
anandgandharwa-create <anandgandharwa@gmail.com>
2026-01-23 19:18:16 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,7 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("zqf-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "about.html",
-        "books.html",
-        "courses.html",
-        "krishna.html",
-        "contact.html"
-      ]);
-    })
-  );
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
 });
